31ff5f0c5091f4ae425070723d69e158d45af7c3
jeherr <johnherr@gmail.com>
2017-07-27 17:11:25 -0400

    DIIS added to HF, but takes more iterations that without

--- qm6/HF.py
+++ qm6/HF.py
@@ -4,7 +4,10 @@ import psi4
 np.set_printoptions(suppress=True, precision=4)
 
 class HFcalc:
-    def __init__(self, mol_, basis_ = "aug-cc-PVDZ"):
+    def __init__(self, mol_, basis_ = "aug-cc-PVDZ", DIIS_ = False):
+        self.DIIS = DIIS_
+        if self.DIIS:
+            self.diis_vectors = 7
         self.mol = mol_
         self.basis = psi4.core.BasisSet.build(self.mol, target=basis_)
         self.mints = psi4.core.MintsHelper(self.basis)
@@ -49,7 +52,23 @@ class HFcalc:
         C = self.A @ Cp
         return eps, C
 
+    def DIIS_step(self):
+        B = -1*np.ones((self.diis_vectors+1, self.diis_vectors+1))
+        B[-1,-1] = 0
+        for i in range(self.diis_vectors):
+            for j in range(i, self.diis_vectors):
+                ri_dot_rj = np.dot(self.r_array[i].flatten(),self.r_array[j].flatten())
+                B[i,j] = B[j,i] = ri_dot_rj
+        vec = np.zeros((self.diis_vectors+1))  # [:,None]])
+        vec[-1] = -1
+        coeff =  np.linalg.solve(B, vec)
+        coeff = coeff[:-1]
+        return np.einsum("i,ijk->jk", coeff, self.fock_array)
+
     def SCF(self):
+        if self.DIIS:
+            self.fock_array = np.zeros([self.diis_vectors]+list(self.H.shape))
+            self.r_array = np.zeros([self.diis_vectors]+list(self.H.shape))
         eps, C = self.diag(self.H)
         Cocc = C[:, :self.nel]
         D = Cocc @ Cocc.T
@@ -76,6 +95,21 @@ class HFcalc:
 
             grad_rms = np.mean(grad ** 2) ** 0.5
 
+            if self.DIIS:
+                if iteration < self.diis_vectors:
+                    self.fock_array[iteration] = F
+                    # r = self.A.T @ grad @ self.A
+                    # print(r)
+                    self.r_array[iteration] = self.A.T @ grad @ self.A
+                else:
+                    self.fock_array = np.roll(self.fock_array, -1, axis=0)
+                    self.r_array = np.roll(self.fock_array, -1, axis=0)
+                    self.fock_array[-1] = F
+                    self.r_array[-1] = self.A.T @ grad @ self.A
+                if iteration > 4:
+                    F = self.DIIS_step()
+
+
             # Build the energy
             E_electric = np.sum((F + self.H) * D)
             E_total = E_electric + self.mol.nuclear_repulsion_energy()
@@ -92,6 +126,51 @@ class HFcalc:
             eps, C = self.diag(F)
             Cocc = C[:, :self.nel]
             D = Cocc @ Cocc.T
+            # print(F)
+
+            # if self.DIIS:
+            #     if iteration < self.diis_vectors:
+            #         self.fock_array[iteration] = F
+            #         # r = self.A.T @ grad @ self.A
+            #         # print(r)
+            #         self.r_array[iteration] = self.A.T @ grad @ self.A
+            #     else:
+            #         self.fock_array = np.roll(self.fock_array, -1, axis=0)
+            #         self.r_array = np.roll(self.fock_array, -1, axis=0)
+            #         self.fock_array[-1] = F
+            #         self.r_array[-1] = self.A.T @ grad @ self.A
+            #     # B = np.dot(r.T,r)
+            #     # print(B.shape)
+            #     # print(np.ones(B.shape[1])[None,:])
+            #
+            #     # B = np.r_[B,-np.ones(len(B[:,0]))]
+            #     # B = np.c_[B,-np.ones(len(B[0,:]))]
+            #     # B = np.r_[B,-np.ones(B.shape[1])[None,:]]
+            #     # B[-1,-1] = 0.
+            #     if iteration > 5:
+            #         F = self.DIIS_step()
+            #         # print(F)
+            #         # fock_list.pop(0)
+            #         # r_list.pop(0)
+            #         # B = np.dot(r.T,r)
+            #         # # print(B.shape)
+            #         # # print(np.ones(B.shape[1]))
+            #         # B = np.c_[B,-np.ones(len(B[0,:]))]
+            #         # B = np.r_[B,-np.ones(B.shape[1])[None,:]]
+            #         # B[-1,-1] = 0.
+            #         # # print(B.shape)
+            #         # vec = np.zeros(B.shape[1])  # [:,None]])
+            #         # # vec.pop(0)
+            #         # vec[-1] = -1
+            #         # # print(vec)
+            #         # coeff =  np.linalg.solve(B, vec)
+            #         # np.sum(coeff)
+            #         # # print()
+            #         # # print(coeff)
+            #         # # print(np.sum(coeff))
+            #         # coeff = coeff[:-1]
+            #         # # print(len(coeff))
+            #         # F = np.dot(coeff,F)
         print("SCF has finished!\n")
         return E_total
 
--- tmp.py
+++ tmp.py
@@ -7,6 +7,6 @@ H 1 1.1 2 104
 """)
 mol.update_geometry()
 
-calc = qm6.HF.HFcalc(mol)
+calc = qm6.HF.HFcalc(mol, DIIS_=True)
 print(calc.SCF())
 print(qm6.HF.psi4_energy(mol))
